c5e3e34b328d09105c61021a07904ca2cf8799bc
karelinovian <106594544+karelinovian@users.noreply.github.com>
2022-05-31 14:32:11 +0300

    Start

---
+++ 4g_Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4g_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+                        * Пользователь вводит номер планеты Солнечной системы
+                      - (счет от 1 до 8 по порядку начиная с Меркурия) . 
+
+                      Необходимо вывести на экран его название.
+                       */
+            string Mercury = "Mercury";
+            string Venus = "Venus";
+            string Earth = "Earth";
+            string Mars = "Mars";
+            string Jupiter = "Jupiter";
+            string Saturn = "Saturn";
+            string Uranus = "Uranus";
+            string Neptune = "Neptune";
+
+            int numberPlanet = Convert.ToInt32(Console.ReadLine());
+
+            if (numberPlanet == 1)
+            {
+                Console.WriteLine(Mercury);
+            }
+            else if (numberPlanet == 2)
+            {
+                Console.WriteLine();
+            }
+           }
+      }
+ }
+
